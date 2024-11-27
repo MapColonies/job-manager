1627ee417af2d0de6033b9f1dfa66cd09ea9c454
razbroc <razbroc@gmail.com>
2024-11-27 15:02:57 +0200

    feat: upgrade migration

--- src/DAL/migration/v3.0.0.sql
+++ src/DAL/migration/v3.0.0.sql
@@ -1,20 +1,5 @@
-SET search_path TO "JobManager", public; -- CHANGE SCHEMA NAME TO MATCH ENVIRONMENT
-
-WITH marked_for_delete AS (
-	SELECT unnest((array_agg(id))[2:]) id
-	FROM "Job"
-	WHERE ((status = 'Pending' OR status = 'In-Progress') AND ("additionalIdentifiers" IS NULL)) 
-	GROUP BY "resourceId","version","type"
-), deleteTasks as (
-	DELETE FROM "Task"
-	USING marked_for_delete m
-	WHERE "Task"."jobId" = m.id
-)
-DELETE FROM "Job"
-USING marked_for_delete m
-WHERE "Job".id = m.id;
-
 -- Alter the table to add the modified and new constraints
+ALTER TABLE "Job" ADD COLUMN "suspendedTasks" INT DEFAULT 0 NOT NULL;
 
 ALTER TABLE "Job" 
 DROP CONSTRAINT IF EXISTS "UQ_uniqueness_on_active_tasks";
@@ -27,3 +12,43 @@ DROP CONSTRAINT IF EXISTS "UQ_uniqueness_on_active_tasks_no_additional_identifie
 
 ALTER TABLE "Job"
 ADD CONSTRAINT "UQ_uniqueness_on_active_tasks_no_additional_identifier" EXCLUDE ("resourceId" with =, "version" with =, "type" with =) WHERE ((status = 'Pending' OR status = 'In-Progress' OR status = 'Suspended') AND ("additionalIdentifiers" IS NULL));
+
+ALTER TYPE "operation_status_enum" ADD VALUE 'Suspended'
+
+CREATE OR REPLACE FUNCTION update_tasks_counters_insert() RETURNS trigger
+    SET search_path FROM CURRENT
+    LANGUAGE plpgsql
+    AS $$
+BEGIN
+  UPDATE "Job" 
+  SET "taskCount" = "taskCount" + 1, 
+    "completedTasks" = "completedTasks" + CASE WHEN NEW."status" = 'Completed' THEN 1 ELSE 0 END,
+    "failedTasks" = "failedTasks" + CASE WHEN NEW."status" = 'Failed' THEN 1 ELSE 0 END,
+    "expiredTasks" = "expiredTasks" + CASE WHEN NEW."status" = 'Expired' THEN 1 ELSE 0 END,
+    "pendingTasks" = "pendingTasks" + CASE WHEN NEW."status" = 'Pending' THEN 1 ELSE 0 END,
+    "inProgressTasks" = "inProgressTasks" + CASE WHEN NEW."status" = 'In-Progress' THEN 1 ELSE 0 END,
+    "abortedTasks" = "abortedTasks" + CASE WHEN NEW."status" = 'Aborted' THEN 1 ELSE 0 END,
+    "suspendedTasks" = "suspendedTasks" + CASE WHEN NEW."status" = 'Suspended' THEN 1 ELSE 0 END
+  WHERE id = NEW."jobId";
+  RETURN NULL;
+END;
+$$;
+
+CREATE OR REPLACE FUNCTION update_tasks_counters_delete() RETURNS trigger
+    SET search_path FROM CURRENT
+    LANGUAGE plpgsql
+    AS $$
+BEGIN
+  UPDATE "Job" 
+  SET "taskCount" = "taskCount" - 1, 
+    "completedTasks" = "completedTasks" - CASE WHEN OLD."status" = 'Completed' THEN 1 ELSE 0 END,
+    "failedTasks" = "failedTasks" - CASE WHEN OLD."status" = 'Failed' THEN 1 ELSE 0 END,
+    "expiredTasks" = "expiredTasks" - CASE WHEN OLD."status" = 'Expired' THEN 1 ELSE 0 END,
+    "pendingTasks" = "pendingTasks" - CASE WHEN OLD."status" = 'Pending' THEN 1 ELSE 0 END,
+    "inProgressTasks" = "inProgressTasks" - CASE WHEN OLD."status" = 'In-Progress' THEN 1 ELSE 0 END,
+    "abortedTasks" = "abortedTasks" - CASE WHEN OLD."status" = 'Aborted' THEN 1 ELSE 0 END
+    "suspendedTasks" = "suspendedTasks" - CASE WHEN OLD."status" = 'Suspended' THEN 1 ELSE 0 END
+  WHERE id = OLD."jobId";
+  RETURN NULL;
+END;
+$$;
