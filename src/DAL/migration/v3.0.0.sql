96984bc15ad4c9f61b7c9f814ce650fe0d7d179c
razbroc <razbroc@gmail.com>
2024-11-27 17:43:49 +0200

    fix: fix new migration update counter

--- src/DAL/migration/v3.0.0.sql
+++ src/DAL/migration/v3.0.0.sql
@@ -52,3 +52,25 @@ BEGIN
   RETURN NULL;
 END;
 $$;
+
+
+CREATE OR REPLACE FUNCTION update_tasks_counters_update() RETURNS trigger
+    SET search_path FROM CURRENT
+    LANGUAGE plpgsql
+    AS $$
+BEGIN
+  IF NEW."status" != OLD."status" THEN
+    UPDATE "Job" 
+    SET
+      "completedTasks" = "completedTasks" + CASE WHEN NEW."status" = 'Completed' THEN 1 WHEN OLD."status" = 'Completed' THEN -1 ELSE 0 END,
+      "failedTasks" = "failedTasks" + CASE WHEN NEW."status" = 'Failed' THEN 1 WHEN OLD."status" = 'Failed' THEN -1 ELSE 0 END,
+      "expiredTasks" = "expiredTasks" + CASE WHEN NEW."status" = 'Expired' THEN 1 WHEN OLD."status" = 'Expired' THEN -1 ELSE 0 END,
+      "pendingTasks" = "pendingTasks" + CASE WHEN NEW."status" = 'Pending' THEN 1 WHEN OLD."status" = 'Pending' THEN -1 ELSE 0 END,
+      "inProgressTasks" = "inProgressTasks" + CASE WHEN NEW."status" = 'In-Progress' THEN 1 WHEN OLD."status" = 'In-Progress' THEN -1 ELSE 0 END,
+      "abortedTasks" = "abortedTasks" + CASE WHEN NEW."status" = 'Aborted' THEN 1 WHEN OLD."status" = 'Aborted' THEN -1 ELSE 0 END,
+      "suspendedTasks" = "suspendedTasks" + CASE WHEN NEW."status" = 'Suspended' THEN 1 WHEN OLD."status" = 'Suspended' THEN -1 ELSE 0 END
+    WHERE id = NEW."jobId";
+  END IF;
+  RETURN NULL;
+END;
+$$;
