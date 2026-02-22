58cb039f11f8941c46135692e9433b549fe3a4aa
shlomiko <shlomiko@rafael.co.il>
2026-02-22 13:39:53 +0200

    fix: abort api final status validation

--- src/taskManagement/models/taskManagementManger.ts
+++ src/taskManagement/models/taskManagementManger.ts
@@ -61,11 +61,7 @@ export class TaskManagementManager {
       this.logger.error({ jobId: req.jobId, msg: message });
       throw new NotFoundError(message);
     }
-    if (jobEntity.status === OperationStatus.COMPLETED || jobEntity.status === OperationStatus.ABORTED) {
-      const message = 'Job abort request failed, job status cannot be one of: "Completed" or "Aborted"';
-      this.logger.error({ jobStatus: jobEntity.status, msg: message });
-      throw new BadRequestError(message);
-    }
+
     await jobRepo.updateJob({ jobId: req.jobId, status: OperationStatus.ABORTED });
     const taskRepo = await this.getTaskRepository();
     await taskRepo.abortJobTasks(req.jobId);
